3a433d7e4d23df556400b692b82eb0783610d5b9
Alexandra Weaver <aweaver0124@gmail.com>
2022-01-18 16:01:26 -0600

    commit tree 1657f6acf878ae8c2e76965376aea6380d906e8c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,12 @@
-class Dog
+
+class Dog < ActiveRecord::Base
+
+    attr_accessor :name, :breed
+
+    def initialize(name:, breed:)
+        @name = name
+        @breed = breed
+    end
 end
+
+dog = Dog.create(name: "Ralph", breed: "lab")
